13a83c66fa73ab083455934dce11bc74dc04507d
mrk3969 <kujo40@ptd.net>
2014-09-25 12:45:31 -0400

    Added some functionality to Activity and Tests

--- ActivityApp/DomainModel/Activity.java
+++ ActivityApp/DomainModel/Activity.java
@@ -9,9 +9,6 @@ public class Activity
 	
 	public Activity(String date, String type, int caloriesBurned)
 	{
-		theDate = date;
-		theType = type;
-		theCaloriesBurned = caloriesBurned;
 		
 	}
 	
@@ -27,25 +24,7 @@ public class Activity
 	
 	public boolean changeActivity(Activity activity)
 	{
-		
-		
 		return true;
 	}
 
-	public String getDate() 
-	{
-		return theDate;
-	}
-
-	public String getType() 
-	{
-		return theType;
-	}
-
-	public int getCaloriesBurned() 
-	{
-		return theCaloriesBurned;
-	}
-
 }
-
--- ActivityApp/src/DomainModel/ActivityList.java
+++ ActivityApp/src/DomainModel/ActivityList.java
@@ -1,6 +1,20 @@
 package DomainModel;
 
+import java.util.ArrayList;
+
+import DataMappers.ActivityDataMapper;
+
 public class ActivityList
 {
-
+	public ActivityList()
+	{
+		
+	}
+	
+	public static ArrayList findActivityList()
+	{
+		ActivityDataMapper adm = new ActivityDataMapper();
+		return null;
+		
+	}
 }
