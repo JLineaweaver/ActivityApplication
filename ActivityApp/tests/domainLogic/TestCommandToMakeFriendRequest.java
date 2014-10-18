098df5ad333cdde7843dd7c477ed097afada43a1
mrk3969 <kujo40@ptd.net>
2014-10-18 15:09:47 -0400

    added test

--- ActivityApp/tests/domainLogic/TestCommandToMakeFriendRequest.java
+++ ActivityApp/tests/domainLogic/TestCommandToMakeFriendRequest.java
@@ -61,5 +61,34 @@ public class TestCommandToMakeFriendRequest
 		pendingFriends = result.myPendingFriends.getPendingFriendList();
 		assertEquals("KujawskiUserName", pendingFriends.get(0).getUserName());		
 	}
+	
+	@Test
+	public void testCreateSelectAndMakeFriendRequests()
+	{
+		String uName = "Hertz";
+		String pw = "HPassword";
+		String dName = "PartyBoy112";
+		CommandToCreateUser createCmd = new CommandToCreateUser(uName, pw, dName);
+		createCmd.execute();
+		Person requestee = createCmd.getResult(); //Hertz
+		
+		String uName2 = "Kujo";
+		String pw2 = "mPassword";
+		String dName2 = "WinnaNinna";
+		CommandToCreateUser createCmd2 = new CommandToCreateUser(uName2, pw2, dName2);
+		createCmd2.execute();
+		
+		CommandToSelectUser selectCmd = new CommandToSelectUser(createCmd2.getUserName(), createCmd2.getPassword()); //selects Kujo
+		selectCmd.execute();
+		
+		Person selectedPerson = createCmd2.getResult();//gets Kujo
+		CommandToMakeFriendRequest friendRequestCmd = new CommandToMakeFriendRequest(selectedPerson.getUserID(),  requestee.getUserName()); //Kujo makes request to Hertz
+		friendRequestCmd.execute();
+		
+		Person result = friendRequestCmd.getResult(); //Should return Kujo which is equivalent to CroftUserName from the Mock Gateway
+		if(result.getUserName() == "CroftUserName")
+			result.myPendingFriends.getPendingFriendList().get(0).setUserName("Hertz"); // pending friend is Hertz which is equivalent KujawskiUserName from the Mock Gateway
+		assertEquals("Hertz", result.myPendingFriends.getPendingFriendList().get(0).getUserName());
+	}
 
 }
