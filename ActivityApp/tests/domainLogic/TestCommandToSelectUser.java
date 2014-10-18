c4b646949784770d9ea17dca990ea223bd88a7b8
mrk3969 <kujo40@ptd.net>
2014-10-18 13:15:07 -0400

    Added more tests

--- ActivityApp/tests/domainLogic/TestCommandToSelectUser.java
+++ ActivityApp/tests/domainLogic/TestCommandToSelectUser.java
@@ -28,4 +28,42 @@ public class TestCommandToSelectUser
 		assertEquals("MockUserName", selectedPerson.getUserName());
 		assertEquals("MockPassword", selectedPerson.getPassword());
 	}
+	
+	@Test
+	public void testSelectOneFromTwoPeople()
+	{
+		String uName = "Johnny";
+		String pw = "JPassword";
+		String dName = "JohnnyJohn";
+		CommandToCreateUser cmd = new CommandToCreateUser(uName, pw, dName);
+		cmd.execute();
+		String uName2 = "Matthew";
+		String pw2 = "password";
+		String dName2 = "MattyMatt3000";
+		CommandToCreateUser cmd2 = new CommandToCreateUser(uName2, pw2, dName2);
+		cmd2.execute();
+		CommandToSelectUser cmd3 = new CommandToSelectUser(cmd.getUserName(), cmd.getPassword());
+		cmd3.execute();
+		Person selectedPerson = cmd.getResult();
+		assertEquals("Johnny", selectedPerson.getUserName());	
+	}
+	
+	@Test
+	public void testSelectOtherFromTwoPeople()
+	{
+		String uName = "Johnny";
+		String pw = "JPassword";
+		String dName = "JohnnyJohn";
+		CommandToCreateUser cmd = new CommandToCreateUser(uName, pw, dName);
+		cmd.execute();
+		String uName2 = "Matthew";
+		String pw2 = "password";
+		String dName2 = "MattyMatt3000";
+		CommandToCreateUser cmd2 = new CommandToCreateUser(uName2, pw2, dName2);
+		cmd2.execute();
+		CommandToSelectUser cmd3 = new CommandToSelectUser(cmd2.getUserName(), cmd2.getPassword());
+		cmd3.execute();
+		Person selectedPerson = cmd2.getResult();
+		assertEquals("Matthew", selectedPerson.getUserName());	
+	}
 }
